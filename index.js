38f8fc2535cf91921ccb8f608701a122eb79eacf
Juan Batiz-Benet <juan@benet.ai>
2014-06-11 14:33:39 -0700

    cleanup

--- index.js
+++ index.js
@@ -19,31 +19,7 @@ Loader.autoInstall = false;
 Loader.LoadId = function(id) {
   var name = Loader.NpmName(id)
   return require(name);
-
-  // try npm -- worry about this later.
-  // return Loader.LoadFromNpm(id);
 }
-
-Loader.LoadFromNpm = function (id) {
-  name = Loader.NpmName(id)
-
-  // worry about this later.
-  // if (Loader.autoInstall) {
-  //   install(argv.install)
-  // };
-
-  return require(name);
-}
-
-// Loader.InstallFromNpm = function(npmName) {
-//   if (!npmName)
-//     throw new Error('install requires valid npmName');
-
-//   install(npmName, function(err, data) {
-//     if (err) throw err;
-//     callback(data);
-//   });
-// }
 
 Loader.NpmName = function(id) {
   return 'transformer.' + id;
