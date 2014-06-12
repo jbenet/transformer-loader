5aa45af33c8eb957f640f92b4db2e277e7de2fdc
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 02:00:04 -0700

    cleaned up api

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 var unique = require('lodash.uniq')
 var filter = require('lodash.filter')
 var isString = require('lodash.isstring')
-// var install = require('npm-installer');
+var npmDir = require('npm-dir')
+var npmResolve = require('resolve')
+
 module.exports = Loader;
 
-function Loader(id) {
+function Loader(id, useGlobal) {
   if (!isString(id) || !(id.length > 0))
     throw new Error('id must be a nonempty string. Got '+id);
 
-  if (!Loader.cache[id])
-    Loader.cache[id] = Loader.LoadId(id)
+  if (!Loader.cache[id]) {
+    var name = Loader.npmName(id)
+    Loader.cache[id] = Loader.loadModule(name, useGlobal)
+  }
   return Loader.cache[id]
 }
 
+// local cache of loaded modules (based on id).
+// Note: this may not be needed -- require has its own...
 Loader.cache = {}
-Loader.autoInstall = false;
 
-Loader.LoadId = function(id) {
-  var name = Loader.NpmName(id)
-  return require(name);
-}
-
-Loader.NpmName = function(id) {
+// transformer id to npm module name
+Loader.npmName = function(id) {
   return 'transformer.' + id;
 }
 
@@ -43,4 +44,32 @@ Loader.missingModules = function(ids) {
       throw e;
     }
   }));
+}
+
+Loader.loadModule = function(name, useGlobal) {
+  if (!useGlobal)
+    return require(name)
+
+  return Loader.globalLoadModule(name)
+}
+
+// special global loading, uses npm modules to resolve.
+// See https://github.com/jbenet/transformer/issues/15
+Loader.globalLoadModule = function(name) {
+  try {
+    // try simple require first
+    return require(name)
+
+  } catch (e) {
+
+    // if error is NOT about module not found, re-throw it.
+    if (!Loader.errIsModuleNotFound(e))
+      throw e
+
+    // if it is, try global installation
+    var res = npmResolve.sync(name, { basedir: npmDir.dir })
+    if (res) {
+      return require(res);
+    }
+  }
 }
